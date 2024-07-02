49e21a5d63c3b78d1d767b334c12751e53b8a44e
costantinoai <59078281+costantinoai@users.noreply.github.com>
2024-07-02 14:57:44 +0200

    edited sh file

--- fetch_and_send.sh
+++ fetch_and_send.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/costantino_ai/miniconda3/etc/profile.d/conda.sh  # Use the correct path to conda.sh
-conda activate scrape
+conda activate scholarbot
 cd /home/eik-tb/costantino_ai/GitHub/scholar-slack-bot/
 python main.py --verbose
