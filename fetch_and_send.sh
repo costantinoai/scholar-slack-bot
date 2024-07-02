21cab8f40f19750c1ebfa2a5d069dc153f1f1959
costantinoai <59078281+costantinoai@users.noreply.github.com>
2024-07-02 14:50:28 +0200

    Update .gitignore to exclude fetch_and_send.sh

--- fetch_and_send.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/eik-tb/miniconda3/etc/profile.d/conda.sh  # Use the correct path to conda.sh
-conda activate scrape
-cd /home/eik-tb/OneDrive_andreaivan.costantino@kuleuven.be/GitHub/scholar-slack-bot/
-python main.py --verbose
