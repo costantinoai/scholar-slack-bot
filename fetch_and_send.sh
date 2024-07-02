3823b99d1603a8e4c8e7a80a7a4bc0f2dce107bf
costantinoai <59078281+costantinoai@users.noreply.github.com>
2024-07-02 14:30:53 +0200

    Update fetch_and_send.sh

--- fetch_and_send.sh
+++ fetch_and_send.sh
@@ -3,5 +3,3 @@ source /home/eik-tb/miniconda3/etc/profile.d/conda.sh  # Use the correct path to
 conda activate scrape
 cd /home/eik-tb/OneDrive_andreaivan.costantino@kuleuven.be/GitHub/scholar-slack-bot/
 python main.py --verbose
-
-
